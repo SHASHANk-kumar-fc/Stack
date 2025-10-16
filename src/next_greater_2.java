7eff3212727abcb3172bdb77b3deb7b00c106d3e
HP <ts875285@gamil.com>
2025-10-16 17:07:43 +0530

    Updated next_greater_2.java with solution

--- src/next_greater_2.java
+++ src/next_greater_2.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
-public class next_greater_2 {
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int ans[] =new int[n];
+        Arrays.fill(ans,-1);
+        Stack<Integer>s=new Stack<>();
+        for(int i=(n-1)*2;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<=nums[i%n])
+                s.pop();
+            if(!s.isEmpty() && i<n )
+                ans[i]=s.peek();
+            s.push(nums[i%n]);
+        }return ans;
+    }
 }
